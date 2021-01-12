fd8e7be84aace8148440061a6218a3f433cc44f0
Ito Zapata <ito@dashbot.io>
2021-01-12 14:55:06 -0800

    setting up exports for first test.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-exports.handler = {
-
+module.exports = {
+  isAllowed: require('./is-allowed'),
+  constants: require('./permission-contstants'),
+  getPermissionsMap: require('./get-resource-permissions-map'),
+  getUser: require('./get-user'),
+  botAuthorization:require('./authorizers/bot-authorization')
 }
